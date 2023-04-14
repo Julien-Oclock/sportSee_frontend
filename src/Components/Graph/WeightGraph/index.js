fec660a5ccef0af128ec2f5455c41084126ae4b6
Julien Levieux <julien.levieux@drotek.com>
2023-04-14 12:28:02 +0200

    update weightChart

--- src/Components/Graph/WeightGraph/index.js
+++ src/Components/Graph/WeightGraph/index.js
@@ -28,94 +28,94 @@ const CustomTooltip = ({ active, payload }) => {
 const WeightGraph = ({ sessions }) => {    
     return (
     <div className="weight-wrapper">
-    <h3 className="weight-title">Activité quotidienne</h3>
-    <ResponsiveContainer className="weight-graph" width={"100%"} height={350}>
-      <BarChart
-        barGap={10}
-        data={sessions}
-        margin={{
-            top: 100,
-            bottom: 0,
-            left : 50,
-        }}
-      >
-        <CartesianGrid strokeDasharray="2" vertical={false} />
-        <XAxis
-          tickLine={false}
-          tickMargin={15}
-          dataKey="day"
-          tickFormatter={(date) => new Date(date).getDate()}
-          stroke="#9B9EAC"
-        />
-        <YAxis
-          yAxisId={"kilogramAxisId"}
-          axisLine={false}
-          tickLine={false}
-          tickCount={3}
-          dataKey="kilogram"
-          type="number"
-          orientation="right"
-          tickMargin={15}
-          stroke="#9B9EAC"
-        />
-        <YAxis
-          tick={false}
-          yAxisId={"caloriesAxisId"}
-          axisLine={false}
-          dataKey="calories"
-          orientation="right"
-        />
-        <Tooltip
-          allowEscapeViewBox={{
-            x: true,
-            y: true,
+      <h3 className="weight-title">Activité quotidienne</h3>
+      <ResponsiveContainer className="weight-graph" width={"100%"} height={350}>
+        <BarChart
+          barGap={10}
+          data={sessions}
+          margin={{
+              top: 100,
+              bottom: 0,
+              left : 50,
           }}
-          position={{ y: -50 }}
-          wrapperStyle={{
-            background: "#E60000",
-            border: "none",
-            outline: "none",
-            position: "absolute",
-            top: "50%",
-            left: "0",
-            fontSize: '9px'
-          }}
-          content={<CustomTooltip />}
-        />
-        <Bar
-          yAxisId={"kilogramAxisId"}
-          dataKey="kilogram"
-          barSize={10}
-          radius={[50, 50, 0, 0]}
-          fill="#282D30"
-        />
-        <Bar
-          yAxisId={"caloriesAxisId"}
-          dataKey="calories"
-          barSize={10}
-          radius={[50, 50, 0, 0]}
-          fill="#E60000"
-        />
-        <Legend
-            width={250}
-            wrapperStyle={{ top: 30, left: '60%' }}
-            iconType="circle"
-            content={props => {
-                const { payload } = props;
-                return (
-                    <div className="legend">
-                        {payload.map((entry, index) => (
-                            <p key={`item-${index}`} className="legend-item">
-                                <div className={entry.value === 'calories' ? 'dot-red' : 'dot-black'}> </div>
-                                {entry.value === 'calories' ? 'Calories brûlées (Kcal)' : 'Poids (Kg)'}
-                            </p>
-                        ))}
-                    </div>
-                );
+        >
+          <CartesianGrid strokeDasharray="2" vertical={false} />
+          <XAxis
+            tickLine={false}
+            tickMargin={15}
+            dataKey="day"
+            tickFormatter={(date) => new Date(date).getDate()}
+            stroke="#9B9EAC"
+          />
+          <YAxis
+            yAxisId={"kilogramAxisId"}
+            axisLine={false}
+            tickLine={false}
+            tickCount={3}
+            dataKey="kilogram"
+            type="number"
+            orientation="right"
+            tickMargin={15}
+            stroke="#9B9EAC"
+          />
+          <YAxis
+            tick={false}
+            yAxisId={"caloriesAxisId"}
+            axisLine={false}
+            dataKey="calories"
+            orientation="right"
+          />
+          <Tooltip
+            allowEscapeViewBox={{
+              x: true,
+              y: true,
             }}
-        />
-      </BarChart>
-    </ResponsiveContainer>
+            position={{ y: -50 }}
+            wrapperStyle={{
+              background: "#E60000",
+              border: "none",
+              outline: "none",
+              position: "absolute",
+              top: "50%",
+              left: "0",
+              fontSize: '9px'
+            }}
+            content={<CustomTooltip />}
+          />
+          <Bar
+            yAxisId={"kilogramAxisId"}
+            dataKey="kilogram"
+            barSize={10}
+            radius={[50, 50, 0, 0]}
+            fill="#282D30"
+          />
+          <Bar
+            yAxisId={"caloriesAxisId"}
+            dataKey="calories"
+            barSize={10}
+            radius={[50, 50, 0, 0]}
+            fill="#E60000"
+          />
+          <Legend
+              width={250}
+              wrapperStyle={{ top: 30, left: '60%' }}
+              iconType="circle"
+              content={props => {
+                  const { payload } = props;
+                  return (
+                      <div className="legend">
+                          {payload.map((entry, index) => (
+                              <p key={`item-${index}`} className="legend-item">
+                                  <div className={entry.value === 'calories' ? 'dot-red' : 'dot-black'}> </div>
+                                  {entry.value === 'calories' ? 'Calories brûlées (Kcal)' : 'Poids (Kg)'}
+                              </p>
+                          ))}
+                      </div>
+                  );
+              }}
+          />
+        </BarChart>
+      </ResponsiveContainer>
     </div>
 
     )
